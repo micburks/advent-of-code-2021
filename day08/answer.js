0c13964685978abf2455d400cc7362ba0f66d81a
Mickey Burks <brks.mck@gmail.com>
2021-12-14 09:24:45 -0600

    Cleanup day 8 with functional helpers

--- day08/answer.js
+++ day08/answer.js
@@ -33,82 +33,74 @@ Results (${entries.length}):
 
 // part 2
 function getOutputValue({input, output}) {
+  // digit contains n segments
+  function length(n) {
+    return (d) => d.length === n;
+  }
+  // digit includes all segments from digit n
+  function includesAllSegments(n) {
+    const num = digitMap.get(n);
+    return (d) => num.every((s) => d.includes(s));
+  }
+  // digit does not include all segments from digit n
+  function notIncludesAllSegments(n) {
+    const num = digitMap.get(n);
+    return (d) => !num.every((s) => d.includes(s));
+  }
+  // remove digit n segments from digit
+  function removeSegments(n) {
+    const num = digitMap.get(n);
+    return (d) => d.filter((s) => !num.includes(s));
+  }
+  // digit n includes all segments
+  function hasSegments(n) {
+    const num = digitMap.get(n);
+    return (d) => d.every((s) => num.includes(s));
+  }
+  // digit is not digit n1 or digit n2
+  function notDigits(n1, n2) {
+    return (d) => {
+      const notDigit1 = includesAllSegments(n1);
+      const notDigit2 = includesAllSegments(n2);
+      return !(notDigit1(d) || notDigit2(d));
+    };
+  }
+
   const digitMap = new Map();
-  digitMap.set(
-    1,
-    input.find((d) => d.length === 2)
-  );
-  digitMap.set(
-    4,
-    input.find((d) => d.length === 4)
-  );
-  digitMap.set(
-    7,
-    input.find((d) => d.length === 3)
-  );
-  digitMap.set(
-    8,
-    input.find((d) => d.length === 7)
-  );
+  digitMap.set(1, input.find(length(2)));
+  digitMap.set(4, input.find(length(4)));
+  digitMap.set(7, input.find(length(3)));
+  digitMap.set(8, input.find(length(7)));
 
-  const fiveLengths = input.filter((d) => d.length === 5);
-  const sixLengths = input.filter((d) => d.length === 6);
+  const fiveLengths = input.filter(length(5)); // [2,3,5]
+  const sixLengths = input.filter(length(6)); // [6,9,0]
 
-  digitMap.set(
-    3,
-    fiveLengths.find((d) =>
-      digitMap.get(1).every((segment) => d.includes(segment))
-    )
-  );
+  // 3 is only 5-digit that contains all of 1
+  digitMap.set(3, fiveLengths.find(includesAllSegments(1)));
 
-  digitMap.set(
-    6,
-    sixLengths.find(
-      (d) => !digitMap.get(1).every((segment) => d.includes(segment))
-    )
-  );
+  // 9 is only 6-digit that contains all of 3
+  digitMap.set(9, sixLengths.find(includesAllSegments(3)));
 
-  digitMap.set(
-    9,
-    sixLengths.find((d) =>
-      digitMap.get(3).every((segment) => d.includes(segment))
-    )
-  );
+  // 6 is only 6-digit that does not contain all of 1
+  digitMap.set(6, sixLengths.find(notIncludesAllSegments(1)));
 
-  digitMap.set(
-    0,
-    sixLengths.find(
-      (d) =>
-        !(
-          digitMap.get(6).every((segment) => d.includes(segment)) ||
-          digitMap.get(9).every((segment) => d.includes(segment))
-        )
-    )
-  );
+  // 0 is only remaining 6-digit
+  digitMap.set(0, sixLengths.find(notDigits(6, 9)));
 
+  // removing segments of 3 from 5 leaves a single segment found in 9 but not 2
   const uniqueFiveSegment = fiveLengths
-    .map((d) => d.filter((segment) => !digitMap.get(3).includes(segment)))
-    .find(
-      (d) =>
-        d.length !== 0 &&
-        d.every((segment) => digitMap.get(9).includes(segment))
-    );
+    .map(removeSegments(3))
+    .filter((d) => d.length !== 0)
+    .find(hasSegments(9));
 
+  // find this unique segment in 5
   digitMap.set(
     5,
     fiveLengths.find((d) => d.includes(uniqueFiveSegment[0]))
   );
 
-  digitMap.set(
-    2,
-    fiveLengths.find(
-      (d) =>
-        !(
-          digitMap.get(3).every((segment) => d.includes(segment)) ||
-          digitMap.get(5).every((segment) => d.includes(segment))
-        )
-    )
-  );
+  // 2 is only remaining 5-digit
+  digitMap.set(2, fiveLengths.find(notDigits(3, 5)));
 
   const value = output
     .map((digit) =>
